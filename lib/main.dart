616fbeb200215bf121a1abe50d9f338d6d5b8d38
Hesham Mohamed <heshamffgg@gmail.com>
2024-06-20 22:50:04 +0300

    dark theme &add text in splash view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
@@ -12,8 +13,11 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: SplashView(),
+    return  GetMaterialApp(
+      theme:ThemeData.dark().copyWith(
+        scaffoldBackgroundColor:primarycolor,
+      ),
+      home:const SplashView(),
     );
   }
 }
