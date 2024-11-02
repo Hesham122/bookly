292dd26ab25e47809533fc1aaa71a715d7802a5e
Hesham Mohamed <heshamffgg@gmail.com>
2024-11-02 22:00:21 +0200

    implement fetch similar book

--- lib/features/home/data/repos/home_repos_impl.dart
+++ lib/features/home/data/repos/home_repos_impl.dart
@@ -2,7 +2,7 @@ import 'package:bookly_app/core/errors/failure.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/data/models/book/book.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
-import 'package:dartz/dartz.dart';
+import 'package:dartz/dartz.dart'; 
 import 'package:dio/dio.dart';
 
 class HomeReposImpl implements HomeRepo {
@@ -15,7 +15,7 @@ class HomeReposImpl implements HomeRepo {
     try {
       var data = await _apiService.get(
           endpoint:
-              'volumes?q=subject:programming&Filtering=free-ebooks&Sorting=newest');
+              'volumes?q=subject:flutter&Filtering=free-ebooks&Sorting=newest');
       List<Book> booklists = [];
       for (var item in data['items']) {
         booklists.add(Book.fromJson(item));
@@ -23,9 +23,9 @@ class HomeReposImpl implements HomeRepo {
       return right(booklists);
     } catch (e) {
       
-      // if (e is DioException) {
-      //   return left(ServerFailure.fromDioError(e));
-      // }
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
       return left(ServerFailure(e.toString()));
     
     }
@@ -37,6 +37,25 @@ class HomeReposImpl implements HomeRepo {
       var data = await _apiService.get(
           endpoint: 
               'volumes?q=subject:programming&Filtering=free-ebooks');
+      List<Book> booklists = [];
+      for (var item in data['items']) {
+        booklists.add(Book.fromJson(item));
+      }
+      return right(booklists);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+  
+  @override
+  Future<Either<Failure, List<Book>>> fetchSimilarBooks({required String category}) async{
+    try {
+      var data = await _apiService.get(
+          endpoint: 
+              'volumes?q=subject:programming&Filtering=free-ebooks&Sorting=relevance');
       List<Book> booklists = [];
       for (var item in data['items']) {
         booklists.add(Book.fromJson(item));
